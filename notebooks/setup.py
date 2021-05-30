f4eaeb8d6143cb86830a284488660485312dd151
Jamie Cook <jamie.cook@veitchlister.com.au>
2021-05-30 17:28:30 +1000

    notebooks for reading data from DB or Excel and setting up test data from that

---
+++ notebooks/setup.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import json
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+from pandas import json_normalize
+import psycopg2
+import shapely.wkt
+import shapely.wkb
+
+import matplotlib.pyplot as plt
+from ast import literal_eval
+from shapely.geometry import LineString
+
+
+src_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "roads_cba_py")
+if src_dir not in sys.path:
+    sys.path.append(src_dir)
