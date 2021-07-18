21a6e740ce9e4808319bb513a3d5a251f0bdb025
Jamie Cook <jamie.cook@veitchlister.com.au>
2021-07-18 13:55:58 +1000

    setting up pip package

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8-sig") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.readlines()
+
+setuptools.setup(
+    name="roads-cba-py",
+    version="0.1.0",
+    author="Jamie Cook",
+    author_email="jimi.cook@gmail.com",
+    description="A pure python implementation of a CBA analysis for road maintenance prioritisation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vnopenroads/roads-cba-py",
+    project_urls={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['roads_cba_py'],
+    python_requires=">=3.6",
+    install_requires = requirements
+)
